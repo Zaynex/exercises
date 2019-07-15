e9b3f088c49ba15c87820e2b6efc4ceb9c838d6c
zaynex <zaynex@126.com>
2019-07-15 23:21:23 +0800

    feat: add throttle

--- throttle/index.js
+++ throttle/index.js
@@ -1,20 +1,27 @@
-function throttle(func, wait, immediate) {
-    let timestamp, context, timer, result, args;
-    function later() {
-        var last = Date.now() - timestamp;
-        if(last >= wait) {
-            result = func.apply(context, args);
-            timer = setTimeout(later, last - wait)
-        }
-
+function throttle(func, wait) {
+  let timer = null;
+  // 判断是否为第一次执行
+  let firstExcuted = false;
+  // 保存最后一次调用传入的参数
+  let laterArgs = [];
+  return function(...args) {
+      // 1. 立即执行
+      laterArgs = args;
+    if (!firstExcuted) {
+        // 2.绑定 this，支持参数传递
+      func.apply(this, laterArgs);
+      firstExcuted = true;
+    } else {
+      if (timer) {
+        return;
+      }
+      timer = setTimeout(() => {
+        func.apply(this, laterArgs);
+        clearTimeout(timer);
+        timer = null;
+      }, wait);
     }
-    return function() {
-        timestamp = Date.now(),
-        context = this,
-        args = arguments;
-        if(!timer) timer = setTimeout(later, wait);
-        if(immediate && !timer) {
-            result = func.apply(context, args);
-        }
-    }
+  };
 }
+
+module.exports = throttle;
